51334667cac3c4b9ae9e32a71331f4ff8a04899f
nathaneast <ahrl_ahrl@naver.com>
2023-10-03 20:02:16 +0900

    feat: lint, prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,24 @@
-import { Metadata } from "next";
-import { Roboto, Noto_Sans_KR } from "next/font/google"; // Roboto와 한글 NotoSans를 사용합니다.
+import { Metadata } from 'next';
+import { Roboto, Noto_Sans_KR } from 'next/font/google'; // Roboto와 한글 NotoSans를 사용합니다.
+import { Inter } from 'next/font/google';
 
-import SearchIcon from "@mui/icons-material/Search";
-import DomainVerificationIcon from "@mui/icons-material/DomainVerification";
-import SettingsIcon from "@mui/icons-material/Settings";
-import { Inter } from "next/font/google";
+import StockList from './components/StockList';
 
 export const metadata: Metadata = {
-  title: "hoep-stock",
+  title: 'hoep-stock',
 };
 
 export default function Home() {
   return (
     <main>
-      <header className="flex justify-end">
-        <SearchIcon />
-        <DomainVerificationIcon />
-        <SettingsIcon />
-      </header>
-      <div>main</div>
+      <section>
+        <h2>보유 주식</h2>
+        <p>mock원</p>
+        <span>+ mock</span>
+      </section>
+
+      {/* list */}
+      <StockList />
     </main>
   );
 }
